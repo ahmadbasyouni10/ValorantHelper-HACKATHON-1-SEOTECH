603b75df9ab4a42862511f75dbb4943a248b0dfe
chiphamthao <chiphamthao@gmail.com>
2024-06-26 19:27:38 +0700

    add chatgpt api

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from scripts import create_tables, populate_tables
+from scripts import create_tables, populate_tables, deploy_chatgpt
 
 def main():
-    create_tables.create_tables()
-    populate_tables.main()
+    #create_tables.create_tables()
+    #populate_tables.main()
+    deploy_chatgpt.deploy_chatgpt()
 
 if __name__ == '__main__':
     main()
