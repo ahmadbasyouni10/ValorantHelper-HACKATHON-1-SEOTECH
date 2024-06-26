e33e587585b109ef21d6e8eccf2a7bce52e2d0fb
jasante22 <148590236+jasante22@users.noreply.github.com>
2024-06-26 23:35:21 +0000

    tested create_tables method; modified it so function creates dataBase

--- scripts/chatbot_test.py
+++ scripts/chatbot_test.py
@@ -2,6 +2,7 @@
 import unittest
 import sqlite3
 import create_tables, populate_tables
+import os
 
 # BackEnd Testing for the SQL dataBase
 class TestTables(unittest.TestCase):
@@ -18,8 +19,21 @@ class TestTables(unittest.TestCase):
     def test_fetch_(self):
         self.assertIsNotNone(populate_tables.fetch_weapon_wraps_from_api())
     
-    # Table Population from Populate Tables
-    
+    # Table Population from Populate Tables and Create Tables
+    def test_create_tables(self):
+        # Testing to see if 4 tables were created in the database
+        create_tables.create_tables()
+        self.conn = sqlite3.connect('valorant_bot.db')
+        self.cur = self.conn.cursor()
+        sql_query = '''SELECT name FROM sqlite_master WHERE type='table';'''
+        self.cur.execute(sql_query)
+        # create_tables creates 4 tables: agents, tiers, maps, and weapons
+        self.assertEquals(4, len(self.cur.fetchall()))
+        print(self.cur.fetchall())
+
+        # Testing to see if data was put into database
+        
+
     # ChatGPT 
     # def test_Create_Tables():
     # def setUp(self):
