8a5cc5aaa3e21e4d2a83f32d14a55a9c82567356
chiphamthao <chiphamthao@gmail.com>
2024-06-26 19:32:10 +0700

    remove '#' from main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from scripts import create_tables, populate_tables, deploy_chatgpt
 
 def main():
-    #create_tables.create_tables()
-    #populate_tables.main()
+    create_tables.create_tables()
+    populate_tables.main()
     deploy_chatgpt.deploy_chatgpt()
 
 if __name__ == '__main__':
