95ee653cbe1077dc5afe84e7037b7465bfc3feb1
jasante22 <148590236+jasante22@users.noreply.github.com>
2024-06-27 03:55:42 +0000

    populating competitive tiers adds more dta than necessary

--- scripts/chatbot_test.py
+++ scripts/chatbot_test.py
@@ -6,7 +6,7 @@ import os
 
 # BackEnd Testing for the SQL dataBase
 class TestTables(unittest.TestCase):
-    # API fetches from Populate Tables
+    # Valorant API fetches from Populate Tables
     def test_fetch_agents(self):
         self.assertIsNotNone(populate_tables.fetch_agents_from_api())
 
@@ -29,32 +29,32 @@ class TestTables(unittest.TestCase):
         self.cur.execute(sql_query)
         # create_tables creates 4 tables: agents, tiers, maps, and weapons
         self.assertEquals(4, len(self.cur.fetchall()))
-        print(self.cur.fetchall())
 
         # Testing to see if data was put into database
         populate_tables.main()
-        sql_query = '''SELECT * FROM agents'''
+        sql_query = '''SELECT displayName FROM agents'''
         self.cur.execute(sql_query)
-        print(self.cur.fetchone())
+        # Valorant has 24 agents as of 06.26.2024
+        self.assertEqual(24, len(self.cur.fetchall()) - 1)
 
-        sql_query = '''SELECT * FROM competitive_tiers'''
+        sql_query = '''SELECT tierName FROM competitive_tiers WHERE tierName='UNRANKED';'''
         self.cur.execute(sql_query)
-        print(self.cur.fetchone())
-
-        sql_query = '''SELECT * FROM maps'''
+        # Valorant has 9 ranks, with each having 3 subtiers
+        # that said, populate_tables creates adds more data to 
+        # the tierName table than necessary;
+        # ranks get repeated so something to investigate 
+        print(len(self.cur.fetchall()))
+        # self.assertEqual(1, len(self.cur.fetchall()))
+        
+        sql_query = '''SELECT displayName FROM maps'''
         self.cur.execute(sql_query)
-        print(self.cur.fetchone())
+        # Valorant has 16 maps
+        self.assertEqual(16, len(self.cur.fetchall()))
 
-        sql_query = '''SELECT * FROM weapon_wraps'''
+        sql_query = '''SELECT displayName FROM weapon_wraps WHERE displayName='Sakura Sheriff';'''
         self.cur.execute(sql_query)
-        print(self.cur.fetchone())
+        self.assertEqual(1, len(self.cur.fetchall()))
 
-    # ChatGPT 
-    # def test_Create_Tables():
-    # def setUp(self):
-    #     self.conn = create_tables() 
-    
-    # def test_setup(self):
 
         
 if __name__ == '__main__':
