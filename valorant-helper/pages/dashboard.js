687723f74bf817a2b0b403633a40341008f3dfd3
ahmadbasyouni10 <ahmadnayef2004@gmail.com>
2024-06-28 15:05:06 -0400

    Styling

--- valorant-helper/pages/dashboard.js
+++ valorant-helper/pages/dashboard.js
@@ -243,19 +243,19 @@ export default function dashboard() {
                         <Flex p="1em" color="#fff" flexDir="column" h="100%" justify="space-between">
                             <Flex justify="space-between" w="100%" align="flex-start">
                                 <Flex flexDir="column">
-                                    <Text color="gray.400">,,,,,</Text>
-                                    <Text fontWeight="bold" fontSize="xl">,,,,</Text>
+                                    <Text color="gray.400">Bulldog</Text>
+                                    <Text fontWeight="bold" fontSize="xl"></Text>
                                 </Flex>
                                 <Flex align="center">
-                                    <Text></Text>
+                                    <Text>AR</Text>
                                 </Flex>
                             </Flex>
                             <Text mb={4}></Text>
                             <Flex align="flex-end" justify="space-between">
                                 <Flex>
                                     <Flex flexDir="column" mr={4}>
-                                        <Text textTransform="uppercase" fontSize="xs"></Text>
-                                        <Text fontSize="lg"></Text>
+                                        <Text textTransform="uppercase" fontSize="xs">23/23</Text>
+                                        <Text fontSize="lg">RIOT GAMES</Text>
                                     </Flex>
                                     <Flex flexDir="column">
                                         <Text textTransform="uppercase" fontSize="xs"></Text>
@@ -277,23 +277,23 @@ export default function dashboard() {
                         <Flex p="1em" color="#fff" flexDir="column" h="100%" justify="space-between">
                             <Flex justify="space-between" w="100%" align="flex-start">
                                 <Flex flexDir="column">
-                                    <Text color="gray.400">Current Balance</Text>
-                                    <Text fontWeight="bold" fontSize="xl">$350.00</Text>
+                                    <Text color="gray.400"></Text>
+                                    <Text fontWeight="bold" fontSize="xl">Vandal</Text>
                                 </Flex>
                                 <Flex align="center">
-                                    <Text>Rise.</Text>
+                                    <Text>AR</Text>
                                 </Flex>
                             </Flex>
-                            <Text mb={4}>**** **** **** 8956</Text>
+                            <Text mb={4}></Text>
                             <Flex align="flex-end" justify="space-between">
                                 <Flex>
                                     <Flex flexDir="column" mr={4}>
-                                        <Text textTransform="uppercase" fontSize="xs">Valid Thru</Text>
-                                        <Text fontSize="lg">9/24</Text>
+                                        <Text textTransform="uppercase" fontSize="xs">32/32</Text>
+                                        <Text fontSize="lg">RIOTGAMES</Text>
                                     </Flex>
                                     <Flex flexDir="column">
-                                        <Text textTransform="uppercase" fontSize="xs">CVV</Text>
-                                        <Text fontSize="lg">***</Text>
+                                        <Text textTransform="uppercase" fontSize="xs"></Text>
+                                        <Text fontSize="lg"></Text>
                                     </Flex>
                                 </Flex>
                             </Flex>
@@ -311,23 +311,23 @@ export default function dashboard() {
                         <Flex p="1em" color="#fff" flexDir="column" h="100%" justify="space-between">
                             <Flex justify="space-between" w="100%" align="flex-start">
                                 <Flex flexDir="column">
-                                    <Text color="gray.400"></Text>
+                                    <Text color="gray.400">Phantom</Text>
                                     <Text fontWeight="bold" fontSize="xl"></Text>
                                 </Flex>
                                 <Flex align="center">
-                                    <Text>Rise.</Text>
+                                    <Text>SMG</Text>
                                 </Flex>
                             </Flex>
                             <Text mb={4}></Text>
                             <Flex align="flex-end" justify="space-between">
                                 <Flex>
                                     <Flex flexDir="column" mr={4}>
-                                        <Text textTransform="uppercase" fontSize="xs">Valid Thru</Text>
-                                        <Text fontSize="lg">11/22</Text>
+                                        <Text textTransform="uppercase" fontSize="xs">23/23</Text>
+                                        <Text fontSize="lg">RIOT GAMES</Text>
                                     </Flex>
                                     <Flex flexDir="column">
-                                        <Text textTransform="uppercase" fontSize="xs">CVV</Text>
-                                        <Text fontSize="lg">***</Text>
+                                        <Text textTransform="uppercase" fontSize="xs"></Text>
+                                        <Text fontSize="lg"></Text>
                                     </Flex>
                                 </Flex>
                             </Flex>
