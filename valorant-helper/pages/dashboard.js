ebc413fac9d5b2b4eaf9de47f0992be36f64e07f
ahmadbasyouni10 <ahmadnayef2004@gmail.com>
2024-06-27 22:08:54 -0400

    Styling

--- valorant-helper/pages/dashboard.js
+++ valorant-helper/pages/dashboard.js
@@ -51,7 +51,6 @@ export default function dashboard() {
           try {
             const response = await axios.get('http://localhost:5000/maps');
             setMapsData(response.data);
-            console.log('Maps data:', response.data);
           } catch (error) {
             console.error('Error fetching competitive tiers data:', error);
           }
@@ -244,27 +243,25 @@ export default function dashboard() {
                         <Flex p="1em" color="#fff" flexDir="column" h="100%" justify="space-between">
                             <Flex justify="space-between" w="100%" align="flex-start">
                                 <Flex flexDir="column">
-                                    <Text color="gray.400">Current Balance</Text>
-                                    <Text fontWeight="bold" fontSize="xl">$5,750.20</Text>
+                                    <Text color="gray.400">,,,,,</Text>
+                                    <Text fontWeight="bold" fontSize="xl">,,,,</Text>
                                 </Flex>
                                 <Flex align="center">
-                                    <Icon mr={2} as={FiCreditCard} />
-                                    <Text>Rise.</Text>
+                                    <Text></Text>
                                 </Flex>
                             </Flex>
-                            <Text mb={4}>**** **** **** 1289</Text>
+                            <Text mb={4}></Text>
                             <Flex align="flex-end" justify="space-between">
                                 <Flex>
                                     <Flex flexDir="column" mr={4}>
-                                        <Text textTransform="uppercase" fontSize="xs">Valid Thru</Text>
-                                        <Text fontSize="lg">12/23</Text>
+                                        <Text textTransform="uppercase" fontSize="xs"></Text>
+                                        <Text fontSize="lg"></Text>
                                     </Flex>
                                     <Flex flexDir="column">
-                                        <Text textTransform="uppercase" fontSize="xs">CVV</Text>
-                                        <Text fontSize="lg">***</Text>
+                                        <Text textTransform="uppercase" fontSize="xs"></Text>
+                                        <Text fontSize="lg"></Text>
                                     </Flex>
                                 </Flex>
-                                <Icon as={FiCreditCard} />
                             </Flex>
                         </Flex>
                     </Box>
@@ -284,7 +281,6 @@ export default function dashboard() {
                                     <Text fontWeight="bold" fontSize="xl">$350.00</Text>
                                 </Flex>
                                 <Flex align="center">
-                                    <Icon mr={2} as={FiCreditCard} />
                                     <Text>Rise.</Text>
                                 </Flex>
                             </Flex>
@@ -300,7 +296,6 @@ export default function dashboard() {
                                         <Text fontSize="lg">***</Text>
                                     </Flex>
                                 </Flex>
-                                <Icon as={FiCreditCard} />
                             </Flex>
                         </Flex>
                     </Box>
@@ -316,15 +311,14 @@ export default function dashboard() {
                         <Flex p="1em" color="#fff" flexDir="column" h="100%" justify="space-between">
                             <Flex justify="space-between" w="100%" align="flex-start">
                                 <Flex flexDir="column">
-                                    <Text color="gray.400">Current Balance</Text>
-                                    <Text fontWeight="bold" fontSize="xl">$2,150.72</Text>
+                                    <Text color="gray.400"></Text>
+                                    <Text fontWeight="bold" fontSize="xl"></Text>
                                 </Flex>
                                 <Flex align="center">
-                                    <Icon mr={2} as={FiCreditCard} />
                                     <Text>Rise.</Text>
                                 </Flex>
                             </Flex>
-                            <Text mb={4}>**** **** **** 8353</Text>
+                            <Text mb={4}></Text>
                             <Flex align="flex-end" justify="space-between">
                                 <Flex>
                                     <Flex flexDir="column" mr={4}>
@@ -336,7 +330,6 @@ export default function dashboard() {
                                         <Text fontSize="lg">***</Text>
                                     </Flex>
                                 </Flex>
-                                <Icon as={FiCreditCard} />
                             </Flex>
                         </Flex>
                     </Box>
@@ -349,12 +342,12 @@ export default function dashboard() {
                 </Flex>
                 <Flex flexDir="column" my={4}>
                     <Flex justify="space-between" mb={2}>
-                        <Text>Balance</Text>
-                        <Text fontWeight="bold">$140.42</Text>
+                        <Text>Ammo</Text>
+                        <Text fontWeight="bold">?</Text>
                     </Flex>
                     <Flex justify="space-between">
-                        <Text>Credit Limit</Text>
-                        <Text fontWeight="bold">$150.00</Text>
+                        <Text>Damage</Text>
+                        <Text fontWeight="bold">?</Text>
                     </Flex>
                 </Flex>
                 <br />
